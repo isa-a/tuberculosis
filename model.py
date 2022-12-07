5fc71e1c6c4a97b7e8bdf0494dd430ba34c9c7b8
isa-a <ia19@ic.ac.uk>
2022-12-07 14:17:46 +0000

    Latest

--- model.py
+++ model.py
@@ -17,10 +17,11 @@ I0, R0 = 10, 0
 # Everyone else, S0, is susceptible to infection initially.
 U0 = N - I0 - R0
 Prevalence = I0
+Lf0, Ls0 = 5, 0
 # Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
-beta, gamma = 1.47617188, 1/7
+beta, gamma = 0.98, 1/7
 lamda = beta * I0
-clamda = lamda
+clamda = 0.2 * lamda
 mu, muTB, sigma, rho = 1/80, 1/6, 1/6, 0.03
 u, v, w = 0.083, 0.88, 0.0006
 t = np.linspace(0, 600, 600+1)
@@ -28,7 +29,7 @@ t = np.linspace(0, 600, 600+1)
 # The SIR model differential equations.
 def deriv(y, t, N, beta, gamma, lamda, clamda, mu, muTB, sigma, rho, u, v, w):
     U, Lf, Ls, I, R, Prevalence = y
-    b = mu(U + Lf + Ls + R) + muTB
+    b = (mu * (U + Lf + Ls + R)) + muTB
     dU = b - ((lamda + mu) * U)
     dLf = (lamda*U) + ((clamda)*(Ls + R)) - ((u + v + mu) * Lf)
     dLs = (u * Lf) - ((w + clamda + mu) * Ls)
@@ -39,9 +40,32 @@ def deriv(y, t, N, beta, gamma, lamda, clamda, mu, muTB, sigma, rho, u, v, w):
 
 # Initial conditions are S0, I0, R0
 # Integrate the SIR equations over the time grid, t.
-solve = odeint(deriv, (U0, I0, R0, Prevalence), t, args=(N, beta, gamma, lamda, clamda, mu, muTB, sigma, rho, u, v, w))
+solve = odeint(deriv, (U0, Lf0, Ls0, I0, R0, Prevalence), t, args=(N, beta, gamma, lamda, clamda, mu, muTB, sigma, rho, u, v, w))
 U, Lf, Ls, I, R, Prevalence = solve.T
 
+J_diff = Prevalence[1:] - Prevalence[:-1]
+J_diff = np.diff(Prevalence)
+# Plot the data on three separate curves for S(t), I(t) and R(t)
+fig = plt.figure(facecolor='w')
+ax = fig.add_subplot(111, facecolor='#dddddd', axisbelow=True)
+ax.plot(t, U/100000, 'b', alpha=1, lw=2, label='Susceptible')
+ax.plot(t, Lf/100000, 'r', alpha=1, lw=2, label='Infected')
+ax.plot(t, Ls/100000, 'black', alpha=1, lw=2, label='Recovered')
+ax.plot(t, I/100000, 'green', alpha=1, lw=2, label='Incidence')
+ax.plot(t, R/100000, 'red', alpha=1, lw=2, label='Cumulative incidence')
+ax.plot(t, Prevalence/100000, 'red', alpha=1, lw=2, label='Cumulative incidence')
+#ax.plot(t[1:], J_diff, 'blue', alpha=1, lw=2, label='Daily incidence')
+ax.set_xlabel('Time in days')
+ax.set_ylabel('Number')
+#ax.set_ylim(0,1.1)
+#ax.yaxis.set_tick_params(length=0)
+#ax.xaxis.set_tick_params(length=0)
+ax.grid(b=True, which='major', c='w', lw=2, ls='-')
+legend = ax.legend()
+legend.get_frame().set_alpha(0.5)
+#for spine in ('top', 'right', 'bottom', 'left'):
+#    ax.spines[spine].set_visible(False)
+plt.show()
 
 
 # Total population, N.
