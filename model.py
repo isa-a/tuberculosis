0158960b986b2a98a4e7da0d12025658d7ac2849
isa-a <ia19@ic.ac.uk>
2022-12-12 14:03:00 +0000

    Latest

--- model.py
+++ model.py
@@ -24,7 +24,7 @@ u, v, w = 0.083, 0.88, 0.0006
 t = np.linspace(0, 600, 600+1)
 
 # The SIR model differential equations.
-def deriv(y, t, N, beta, gamma, mu, muTB, sigma, rho, u, v, w):
+def deriv(y, t, N, beta, gamma, mu, muTB, sigma, rho, tau, u, v, w):
     U, Lf, Ls, I, R = y
     b = (mu * (U + Lf + Ls + R)) + (muTB * I0)
     lamda = beta * (I0)
@@ -38,16 +38,16 @@ def deriv(y, t, N, beta, gamma, mu, muTB, sigma, rho, u, v, w):
 
 
 # Integrate the SIR equations over the time grid, t.
-solve = odeint(deriv, (U0, Lf0, Ls0, I0, R0), t, args=(N, beta, gamma, mu, muTB, sigma, rho, u, v, w))
+solve = odeint(deriv, (U0, Lf0, Ls0, I0, R0), t, args=(N, beta, gamma, mu, muTB, sigma, rho, tau, u, v, w))
 U, Lf, Ls, I, R = solve.T
 
 fig = plt.figure(facecolor='w')
 ax = fig.add_subplot(111, facecolor='#dddddd', axisbelow=True)
-ax.plot(t, U/100000, 'b', alpha=1, lw=2, label='uninfected')
-ax.plot(t, Lf/100000, 'r', alpha=1, lw=2, label='latent fast')
-ax.plot(t, Ls/100000, 'black', alpha=1, lw=2, label='latent slow')
-ax.plot(t, I/100000, 'green', alpha=1, lw=2, label='infected')
-ax.plot(t, R/100000, 'red', alpha=1, lw=2, label='recovered')
+ax.plot(t, U, 'b', alpha=1, lw=2, label='uninfected')
+ax.plot(t, Lf, 'r', alpha=1, lw=2, label='latent fast')
+ax.plot(t, Ls, 'black', alpha=1, lw=2, label='latent slow')
+ax.plot(t, I, 'green', alpha=1, lw=2, label='infected')
+ax.plot(t, R, 'red', alpha=1, lw=2, label='recovered')
 ax.set_xlabel('Time in days')
 ax.set_ylabel('Number')
 ax.grid(b=True, which='major', c='w', lw=2, ls='-')
@@ -94,12 +94,12 @@ J_diff = np.diff(J)
 # Plot the data on three separate curves for S(t), I(t) and R(t)
 fig = plt.figure(facecolor='w')
 ax = fig.add_subplot(111, facecolor='#dddddd', axisbelow=True)
-ax.plot(t, S, 'b', alpha=1, lw=2, label='Susceptible')
+ax.plot(t, S, 'yellow', alpha=1, lw=2, label='Susceptible')
 ax.plot(t, I, 'r', alpha=1, lw=2, label='Infected')
 ax.plot(t, R, 'black', alpha=1, lw=2, label='Recovered')
 ax.plot(t, J, 'green', alpha=1, lw=2, label='Incidence')
 #ax.plot(t, J, 'red', alpha=1, lw=2, label='Cumulative incidence')
-#ax.plot(t[1:], J_diff, 'blue', alpha=1, lw=2, label='Daily incidence')
+ax.plot(t[1:], J_diff, 'blue', alpha=1, lw=2, label='Daily incidence')
 ax.set_xlabel('Time in days')
 ax.set_ylabel('Number')
 #ax.set_ylim(0,1.1)
