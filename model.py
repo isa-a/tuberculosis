54f860fa3e06851fbf0a20d3a83f83aaecce4a9b
isa-a <ia19@ic.ac.uk>
2022-12-14 18:07:57 +0000

    Latest

--- model.py
+++ model.py
@@ -13,42 +13,47 @@ import matplotlib.pyplot as plt
 # Total population, N.
 N = 1
 # Initial number of infected and recovered individuals, I0 and R0.
-I0, R0 = 0.01, 0
+I0, R0 = 0.001, 0
 # Everyone else, S0, is susceptible to infection initially.
 U0 = N - I0 - R0
-Lf0, Ls0 = 0.0001, 0.001
+J0 = I0
+Lf0, Ls0 = 0, 0
 # Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
-beta, gamma = 0.2, 1/11
-mu, muTB, sigma, rho, tau = 1/80, 1/6, 1/6, 0.03, 0.2
+beta, gamma = 12, 2/5
+mu, muTB, sigma, rho = 1/80, 1/6, 1/6, 0.03
 u, v, w = 0.083, 0.88, 0.0006
-t = np.linspace(0, 600, 600+1)
+t = np.linspace(0, 20, 20+1)
 
 # The SIR model differential equations.
-def deriv(y, t, N, beta, gamma, mu, muTB, sigma, rho, tau, u, v, w):
+def deriv(y, t, N, beta, gamma, mu, muTB, sigma, rho, u, v, w):
     U, Lf, Ls, I, R, Prevalence = y
-    b = (mu * (U + Lf + Ls + R)) + (muTB * I0)
-    lamda = beta * (I0)
+    b = (mu * (U + Lf + Ls + R)) + (muTB * I)
+    lamda = beta * I
     clamda = 0.2 * lamda
     dU = b - ((lamda + mu) * U)
     dLf = (lamda*U) + ((clamda)*(Ls + R)) - ((u + v + mu) * Lf)
     dLs = (u * Lf) - ((w + clamda + mu) * Ls)
-    dI = w*Ls + v*Lf - ((gamma + muTB + sigma + tau) * I) + (rho * R)
-    dR = ((gamma + sigma + tau) * I) - ((rho + clamda + mu) * R)
-    Prev = w*Ls + v*Lf + (rho * R)
-    return dU, dLf, dLs, dI, dR, Prev
+    dI = w*Ls + v*Lf - ((gamma + muTB + sigma) * I) + (rho * R)
+    dR = ((gamma + sigma) * I) - ((rho + clamda + mu) * R)
+    cI = w*Ls + v*Lf + (rho * R)
+    return dU, dLf, dLs, dI, dR, cI
 
 
 # Integrate the SIR equations over the time grid, t.
-solve = odeint(deriv, (U0, Lf0, Ls0, I0, R0), t, args=(N, beta, gamma, mu, muTB, sigma, rho, tau, u, v, w))
-U, Lf, Ls, I, R = solve.T
+solve = odeint(deriv, (U0, Lf0, Ls0, I0, R0, J0), t, args=(N, beta, gamma, mu, muTB, sigma, rho, u, v, w))
+U, Lf, Ls, I, R, cInc = solve.T
 
+J_diff = cInc[1:] - cInc[:-1]
+J_diff = np.diff(cInc)
 fig = plt.figure(facecolor='w')
 ax = fig.add_subplot(111, facecolor='#dddddd', axisbelow=True)
-ax.plot(t, U, 'b', alpha=1, lw=2, label='uninfected')
-ax.plot(t, Lf, 'r', alpha=1, lw=2, label='latent fast')
-ax.plot(t, Ls, 'black', alpha=1, lw=2, label='latent slow')
-ax.plot(t, I, 'green', alpha=1, lw=2, label='infected')
-ax.plot(t, R, 'red', alpha=1, lw=2, label='recovered')
+#ax.plot(t, U/100000, 'b', alpha=1, lw=2, label='uninfected')
+#ax.plot(t, Lf/100000, 'r', alpha=1, lw=2, label='latent fast')
+#ax.plot(t, Ls/100000, 'black', alpha=1, lw=2, label='latent slow')
+ax.plot(t, I*100000, 'green', alpha=1, lw=2, label='infected')
+#ax.plot(t, R/100000, 'red', alpha=1, lw=2, label='recovered')
+ax.plot(t[1:], J_diff*100000, 'blue', alpha=1, lw=2, label='Daily incidence')
+#ax.plot(t, cInc, 'red', alpha=1, lw=2, label='Prevalence')
 ax.set_xlabel('Time in days')
 ax.set_ylabel('Number')
 ax.grid(b=True, which='major', c='w', lw=2, ls='-')
@@ -58,83 +63,55 @@ plt.show()
 
 
 
-
-
-
-
-
-
-# Total population, N.
-N = 100000
-# Initial number of infected and recovered individuals, I0 and R0.
-I0, R0 = 10, 0
-# Everyone else, S0, is susceptible to infection initially.
-S0 = N - I0 - R0
-J0 = I0
-# Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
-beta, gamma = 1.47617188, 1/7
-# A grid of time points (in days)
 t = np.linspace(0, 77, 77+1)
 
-# The SIR model differential equations.
-def deriv(y, t, N, beta, gamma):
-    S, I, R, J = y
-    dS = ((-beta * S * I) / N)
-    dI = ((beta * S * I) / N) - (gamma * I)
-    dR = (gamma * I)
-    dJ = ((beta * S * I) / N)
-    return dS, dI, dR, dJ
+def peak_infections(x):
+ 
+    # Weeks for which the ODE system will be solved
+    #weeks = df.Week.to_numpy()
 
-# Initial conditions are S0, I0, R0
-# Integrate the SIR equations over the time grid, t.
-solve = odeint(deriv, (S0, I0, R0, J0), t, args=(N, beta, gamma))
-S, I, R, J = solve.T
+    # Total population, N.
+    N = 100000
+    # Initial number of infected and recovered individuals, I0 and R0.
+    #I0, R0 = 10, 0
+    # Everyone else, S0, is susceptible to infection initially.
+    R0 = 0
+    beta = x[0]
+    I0 = x[1]
+    gamma = x[2]
+    S0 = N - I0 - R0
+    J0 = I0
+    # Contact rate, beta, and mean recovery rate, gamma, (in 1/days).
+    #reproductive no. R zero is beta/gamma
+    #gamma = 1/6 #rate should be in weeks now
+    # A grid of time points 
+    times = np.arange(0,84,7)
 
-J_diff = J[1:] - J[:-1]
-J_diff = np.diff(J)
-# Plot the data on three separate curves for S(t), I(t) and R(t)
-fig = plt.figure(facecolor='w')
-ax = fig.add_subplot(111, facecolor='#dddddd', axisbelow=True)
-ax.plot(t, S, 'yellow', alpha=1, lw=2, label='Susceptible')
-ax.plot(t, I, 'r', alpha=1, lw=2, label='Infected')
-ax.plot(t, R, 'black', alpha=1, lw=2, label='Recovered')
-ax.plot(t, J, 'green', alpha=1, lw=2, label='Incidence')
-#ax.plot(t, J, 'red', alpha=1, lw=2, label='Cumulative incidence')
-ax.plot(t[1:], J_diff, 'blue', alpha=1, lw=2, label='Daily incidence')
-ax.set_xlabel('Time in days')
-ax.set_ylabel('Number')
-#ax.set_ylim(0,1.1)
-#ax.yaxis.set_tick_params(length=0)
-#ax.xaxis.set_tick_params(length=0)
-ax.grid(b=True, which='major', c='w', lw=2, ls='-')
-legend = ax.legend()
-legend.get_frame().set_alpha(0.5)
-#for spine in ('top', 'right', 'bottom', 'left'):
-#    ax.spines[spine].set_visible(False)
-plt.show()
+    # The SIR model differential equations.
+    def deriv(y, times, N, beta, gamma):
+        S, I, R, J = y
+        dS = ((-beta * S * I) / N)
+        dI = ((beta * S * I) / N) - (gamma * I)
+        dR = (gamma * I)
+        dJ = ((beta * S * I) / N) #incidence
+        return dS, dI, dR, dJ
+
+    # Initial conditions are S0, I0, R0
+    # Integrate the SIR equations over the time grid, t.
+    solve = odeint(deriv, (S0, I0, R0, J0), times, args=(N, beta, gamma))
+    S, I, R, J = solve.T
+
+    return I/N
+
+def residual(x, df):
+
+    # Total population,  N.
+    StartingPop = 100000
+    incidence = df.incidence.to_numpy()/StartingPop
+    return np.sum((peak_infections(x,df) - incidence) ** 2)
 
 
-
-#J_diff = J[1:] - J[:-1]
-#J_diff = np.diff(J)
-# Plot the data on three separate curves for S(t), I(t) and R(t)
-fig = plt.figure(facecolor='w')
-ax = fig.add_subplot(111, facecolor='#dddddd', axisbelow=True)
-#ax.plot(t, S, 'b', alpha=1, lw=2, label='Susceptible')
-#ax.plot(t, I, 'r', alpha=1, lw=2, label='Infected')
-#ax.plot(t, R, 'black', alpha=1, lw=2, label='Recovered')
-#ax.plot(t, J, 'green', alpha=1, lw=2, label='Incidence')
-#ax.plot(t, J, 'red', alpha=1, lw=2, label='Cumulative incidence')
-ax.plot(t[1:], J_diff, 'blue', alpha=1, lw=2, label='Daily incidence')
-ax.set_xlabel('Time in days')
-ax.set_ylabel('Number')
-#ax.set_ylim(0,1.1)
-#ax.yaxis.set_tick_params(length=0)
-#ax.xaxis.set_tick_params(length=0)
-ax.grid(b=True, which='major', c='w', lw=2, ls='-')
-legend = ax.legend()
-legend.get_frame().set_alpha(0.5)
-#for spine in ('top', 'right', 'bottom', 'left'):
-#    ax.spines[spine].set_visible(False)
-plt.show()
+x0 = [0.5, 10, 1/6] #beta, i0, gamma
+res = minimize(residual, x0, args=(df), method="Nelder-Mead", options={'fatol':1e-04}).x
+print(res)
 
