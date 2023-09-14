6b112d65772391adfc1d4789f963348597ac66df
isa-a <ia19@ic.ac.uk>
2023-09-14 19:30:10 +0100

    Latest

--- make_model.py
+++ make_model.py
@@ -7,6 +7,7 @@ Created on Wed Sep 13 22:20:56 2023
 
 from get_addresses import i,s,d,lim
 import numpy as np
+import random
 
 
 states = ['U', 'Lf', 'Ls', 'Pf', 'Ps', 'I', 'I2', 'Tx', 'Rlo', 'Rhi', 'R']
@@ -40,8 +41,118 @@ for born in gps_born:
     Rhi = state_values['Rhi']
     R = state_values['R']
 
-import random
+    # Progression from 'fast' latent
+    source = Lf
+    destin = I
+    rate = 0.0826  # Replace with a random number
+    m[destin, source] = m[destin, source] + rate
 
+    source = Pf
+    destin = I2
+    rate = 0.0826 * (1 - 0.6)  # Replace with a random number
+    m[destin, source] = m[destin, source] + rate
+
+    # Stabilization of 'fast' to 'slow' latent
+    source = Lf
+    destin = Ls
+    rate = 0.872  # Replace with a random number
+    m[destin, source] = m[destin, source] + rate
+
+    source = Pf
+    destin = Ps
+    rate = 0.872  # Replace with a random number
+    m[destin, source] = m[destin, source] + rate
+
+    # Reactivation of 'slow' latent
+    source = Ls
+    destin = I
+    rate = 0.0006  # Replace with a random number
+    m[destin, source] = m[destin, source] + rate
+
+    source = Ps
+    destin = I
+    rate = 0.0006*(1-0.6)  # Replace with a random number
+    m[destin, source] = m[destin, source] + rate
+
+    # Initiation of treatment
+    source = I
+    destins = [Tx, Rhi]
+    rates = [gamma,1/6]  # Replace with random numbers
+    m[destins, source] = m[destins, source] + rates
+
+    source = I2
+    destins = [Tx, Rhi]
+    rates = [gamma,1/6]  # Replace with random numbers
+    m[destins, source] = m[destins, source] + rates
+
+    # Treatment completion or interruption
+    source = Tx
+    destins = [Rlo, Rhi]
+    rates = [2, 0.01]  # Replace with random numbers
+    m[destins, source] = m[destins, source] + rates
+
+    # Relapse
+    sources = [Rlo, Rhi, R]
+    destin = I2
+    rates = [0.032, 0.14, 0.0015]  # Replace with a random number
+    m[destin, sources] = m[destin, sources] + rates
+
+    # Stabilization of relapse risk
+    sources = [Rlo, Rhi]
+    destin = R
+    rates = random.uniform(0, 1)  # Replace with a random number
+    m[destin, sources] = m[destin, sources] + rates
+
+    # Initiation of TPT
+    source = Lf
+    destin = Pf
+    rate = random.uniform(0, 1)  # Replace with a random number
+    m[destin, source] = m[destin, source] + rate
+
+    source = Ls
+    destin = Ps
+    rate = random.uniform(0, 1)  # Replace with a random number
+    m[destin, source] = m[destin, source] + rate
+
+    # Case-finding
+    sources = [I, I2]
+    destin = Tx
+    rate = random.uniform(0, 1)  # Replace with a random number
+    m[destin, sources] = m[destin, sources] + rate
+
+    source = I2
+    destin = Tx
+    rate = random.uniform(0, 1)  # Replace with a random number
+    m[destin, source] = m[destin, source] + rate
+
+
+
+
+
+
+
+
+
+r.progression  = 0.0826;
+r.LTBI_stabil  = 0.872;
+r.reactivation = 0.0006;
+
+r.Tx            = 2;
+r.default       = 0.01;
+
+r.self_cure    = 1/6;
+r.relapse      = [0.032 0.14 0.0015];
+% r.relapse      = [0 0 0];
+% r.mu           = 1/66;                                                   % natural mortality
+r.muTB         = 1/6;                                                      % TB related mortality
+p.imm          = 0.8;                                                      % Reduced susceptibility conferred by previous infection
+
+% --- Interventions 
+p.migrTPT      = 0;                                                        % Proportion of migrants initiated on TPT on entry
+p.TPTeff       = 0.6;                                                      % Effectiveness of TPT
+r.TPT          = [0 0];
+r.ACF          = [0 0];
+r.ACF2         = [0 0];
 
 m = np.zeros((i['nstates'], i['nstates']))
     
@@ -61,89 +172,6 @@ for born in gps_born:
         Rhi = state_values['Rhi']
         R = state_values['R']
 
-        # Progression from 'fast' latent
-        source = Lf
-        destin = I
-        rate = random.uniform(0, 1)  # Replace with a random number
-        m[destin, source] = m[destin, source] + rate
-    
-        source = Pf
-        destin = I2
-        rate = random.uniform(0, 1) * (1 - 0.04)  # Replace with a random number
-        m[destin, source] = m[destin, source] + rate
-    
-        # Stabilization of 'fast' to 'slow' latent
-        source = Lf
-        destin = Ls
-        rate = random.uniform(0, 1)  # Replace with a random number
-        m[destin, source] = m[destin, source] + rate
-    
-        source = Pf
-        destin = Ps
-        rate = random.uniform(0, 1)  # Replace with a random number
-        m[destin, source] = m[destin, source] + rate
-    
-        # Reactivation of 'slow' latent
-        source = Ls
-        destin = I
-        rate = random.uniform(0, 1)  # Replace with a random number
-        m[destin, source] = m[destin, source] + rate
-    
-        source = Ps
-        destin = I
-        rate = random.uniform(0, 1)  # Replace with a random number
-        m[destin, source] = m[destin, source] + rate
-    
-        # Initiation of treatment
-        source = I
-        destins = [Tx, Rhi]
-        rates = [random.uniform(0, 1), random.uniform(0, 1)]  # Replace with random numbers
-        m[destins, source] = m[destins, source] + rates
-    
-        source = I2
-        destins = [Tx, Rhi]
-        rates = [random.uniform(0, 1), random.uniform(0, 1)]  # Replace with random numbers
-        m[destins, source] = m[destins, source] + rates
-    
-        # Treatment completion or interruption
-        source = Tx
-        destins = [Rlo, Rhi]
-        rates = [random.uniform(0, 1), random.uniform(0, 1)]  # Replace with random numbers
-        m[destins, source] = m[destins, source] + rates
-    
-        # Relapse
-        sources = [Rlo, Rhi, R]
-        destin = I2
-        rates = random.uniform(0, 1)  # Replace with a random number
-        m[destin, sources] = m[destin, sources] + rates
-    
-        # Stabilization of relapse risk
-        sources = [Rlo, Rhi]
-        destin = R
-        rates = random.uniform(0, 1)  # Replace with a random number
-        m[destin, sources] = m[destin, sources] + rates
-    
-        # Initiation of TPT
-        source = Lf
-        destin = Pf
-        rate = random.uniform(0, 1)  # Replace with a random number
-        m[destin, source] = m[destin, source] + rate
-    
-        source = Ls
-        destin = Ps
-        rate = random.uniform(0, 1)  # Replace with a random number
-        m[destin, source] = m[destin, source] + rate
-    
-        # Case-finding
-        sources = [I, I2]
-        destin = Tx
-        rate = random.uniform(0, 1)  # Replace with a random number
-        m[destin, sources] = m[destin, sources] + rate
-    
-        source = I2
-        destin = Tx
-        rate = random.uniform(0, 1)  # Replace with a random number
-        m[destin, source] = m[destin, source] + rate
     
 
 
@@ -159,3 +187,11 @@ file_path = 'matrix.txt'
 np.savetxt(file_path, m, fmt='%.6f', delimiter='\t')
 
 print(f"Matrix saved to {file_path}")
+
+
+# Get the indices (coordinates) of non-zero elements in 'm'
+non_zero_indices = np.transpose(np.nonzero(m))
+
+# Display the non-zero positions and their coordinates
+for coord in non_zero_indices:
+    print(f"Coordinate: {tuple(coord)}, Value: {m[coord[0], coord[1]]}")
