edd0da6923dedbe1aa729eb294b497ac969a93d5
isa-a <ia19@ic.ac.uk>
2023-09-26 17:05:15 +0100

    Latest

--- gov_eqs_scaleup.py
+++ gov_eqs_scaleup.py
@@ -5,38 +5,36 @@ Created on Tue Jun 14 13:33:40 2023
 @author: ISA
 """
 
-from model_setup import *
-from mcmc import beta_mean, gamma_mean
 import numpy as np
-from scipy.integrate import odeint
-import matplotlib.pyplot as plt
-
-# Define the start and end years
-start = 2000
-end = 2020
-
-# Calculate the number of time steps based on the desired range
-num_years = end_year - start_year + 1  # +1 to include the end year
-num_time_steps = num_years * 365  # Assuming daily time steps
-
-# Create the time array to represent years from 2010 to 2020
-time_range = np.linspace(start, end, num_time_steps)
-
-solve = odeint(gov_eqs, (U0, Lf0, Ls0, I0, R0), time_range, args=(N, beta_mean, gamma_mean, u, v, w))
+from goveqs_basis import goveqs_basis2
+from allocate import allocate_parameters
+from setup_model import r,p,gps_born,i,s
+from make_model import make_model
 
 
-fig = plt.figure(facecolor='w')
-ax = fig.add_subplot(111, facecolor='#dddddd', axisbelow=True)
-#ax.plot(t, U*100000, 'black', alpha=1, lw=2, label='uninfected')
-#ax.plot(t, Lf*100000, 'black', alpha=1, lw=2, label='latent fast')
-#ax.plot(t, Ls*100000, 'purple', alpha=1, lw=2, label='latent slow')
-ax.plot(t, I*100000, 'green', alpha=1, lw=2, label='infected')
-ax.plot(t, R*100000, 'blue', alpha=1, lw=2, label='recovered')
-ax.set_xlabel('Time')
-ax.set_ylabel('Number')
-#ax.set_xlim(2019, 2030)
-ax.grid(which='major', c='w', lw=2, ls='-')
-legend = ax.legend()
-legend.get_frame().set_alpha(0.5)
-#plt.title("Incidence")
-plt.show()
+
+times = [2022, 2025]
+t=np.arange(2022, 2037)
+
+p['migrTPT'] = 0
+p1 = p
+r1 = r
+p1['migrTPT'] = 1
+M0 = make_model(p, r, i, s, gps_born)
+M1 = make_model(p1, r1, i, s, gps_born)
+
+
+def goveqs_scaleup(t, insert, i, s, M0, M1, p0, p1, times, agg, sel, r):
+    #scale = min(max((t - times[0]) / (times[1] - times[0]), 0), 1)
+    scale = np.minimum(1.0, np.maximum(0.0, (t - times[0]) / (times[1] - times[0])))
+
+    
+    Ms = M1.copy()
+    Ms['lin'] = M0['lin'] + scale * (M1['lin'] - M0['lin'])
+    
+    ps = p1.copy()
+    ps['migrTPT'] = p0['migrTPT'] + scale * (p1['migrTPT'] - p0['migrTPT'])
+    
+    out = goveqs_basis2(t, insert, i, s, Ms, agg, sel, r, ps)
+    
+    return out
