fe2a7676869735ea892472655bd1ee75a28d76e7
isa-a <ia19@ic.ac.uk>
2023-09-26 16:17:43 +0100

    Latest

--- goveqs_basis.py
+++ goveqs_basis.py
@@ -10,61 +10,53 @@ from setup_model import i,s,d,lim,r,p,agg,sel,ref,xi,prm,gps_born
 from make_model import make_model
 gps = gps_born
 
-def goveqs_basis2():
-
-    return 
 
 
+def goveqs_basis2(t, insert, i, s, M, agg, sel, r, p):
+        
+    out = np.zeros((len(insert), 1))
+    invec = insert[:i['nstates']]
     
-t = 2022
+    # new infections
+    lam = make_model(p, r, i, s, gps)['lam'] @ (invec.reshape(-1,1)) / np.sum(invec.reshape(-1,1)) * (1 - 0.1473) ** np.maximum((t - 2010), 0)
+    
+    
+    allmat = make_model(p, r, i, s, gps)['lin'] + (lam * make_model(p, r, i, s, gps)['nlin'])
+    out[:i['nstates']] = np.dot(allmat, invec.reshape(-1, 1))
+    
+    # mortality
+    morts = make_model(p, r, i, s, gps)['mort'] * invec.reshape(-1, 1)
+    out[:i['nstates']] -= np.sum(morts, axis=1).reshape(-1, 1)
+    
+    # and births
+    allmorts = np.sum(morts)
+    births = 0.7268 * allmorts
+    out[i[('U', 'dom')]] += births
+    
+    vec = invec[s['dom']]
+    vec[1:3] = vec[1:3] * 12.3779 * (1 - 0)
+    vec[3:5] = vec[3:5] * 12.3779 * 0
+    vec = vec / sum(vec) * (1 - 0.72) * allmorts
+    out[s['for']] += vec.reshape(-1, 1)
+    
+    
+    # aux
+    
+    out[i['aux']['inc']] = agg['inc'] @ (sel['inc'] * allmat) @ invec.reshape(-1, 1)
+    
+    out[i['aux']['sources'][0]] = np.sum((sel['Lf2I'] * allmat) @ invec.reshape(-1, 1))
+    
+    out[i['aux']['sources'][1]] = np.sum((sel['Pf2I'] * allmat) @ invec.reshape(-1, 1))
+    
+    out[i['aux']['sources'][2]] = np.sum((sel['Ls2I'] * allmat) @ invec.reshape(-1, 1))
+    
+    out[i['aux']['sources'][3]] = np.sum((sel['Ps2I'] * allmat) @ invec.reshape(-1, 1))
+    
+    out[i['aux']['sources'][4]] = np.sum((sel['R2I'] * allmat) @ invec.reshape(-1, 1))
+    
+    out[-1] = np.sum(morts[:, 1])
+    return out
 
-insert = np.zeros(i['nx']) # delete when finished
-seed = 1e-5
-insert[i[('U', 'dom')]] = 1 - seed
-insert[i[('I', 'dom')]] = seed
-
-
-out = np.zeros((len(insert), 1))
-invec = insert[:i['nstates']]
-
-# new infections
-lam = make_model(p, r, i, s, gps)['lam'] @ (invec.reshape(-1,1)) / np.sum(invec.reshape(-1,1)) * (1 - 0.1473)
-
-
-allmat = make_model(p, r, i, s, gps)['lin'] + (lam * make_model(p, r, i, s, gps)['nlin'])
-out[:i['nstates']] = np.dot(allmat, invec.reshape(-1, 1))
-
-# mortality
-morts = make_model(p, r, i, s, gps)['mort'] * invec.reshape(-1, 1)
-out[:i['nstates']] -= np.sum(morts, axis=1).reshape(-1, 1)
-
-# and births
-allmorts = np.sum(morts)
-births = 0.7268 * allmorts
-out[i[('U', 'dom')]] += births
-
-vec = invec[s['dom']]
-vec[1:3] = vec[1:3] * 12.3779 * (1 - 0)
-vec[3:5] = vec[3:5] * 11 * 0
-vec = vec / sum(vec) * (1 - 0.72) * allmorts
-out[s['for']] += vec.reshape(-1, 1)
-
-
-# aux
-
-out[i['aux']['inc']] = agg['inc'] @ (sel['inc'] * allmat) @ invec.reshape(-1, 1)
-
-out[i['aux']['sources'][0]] = np.sum((sel['Lf2I'] * allmat) @ invec.reshape(-1, 1))
-
-out[i['aux']['sources'][1]] = np.sum((sel['Pf2I'] * allmat) @ invec.reshape(-1, 1))
-
-out[i['aux']['sources'][2]] = np.sum((sel['Ls2I'] * allmat) @ invec.reshape(-1, 1))
-
-out[i['aux']['sources'][3]] = np.sum((sel['Ps2I'] * allmat) @ invec.reshape(-1, 1))
-
-out[i['aux']['sources'][4]] = np.sum((sel['R2I'] * allmat) @ invec.reshape(-1, 1))
-
-out[i['aux']['mort']] = np.sum(morts[:, 1])
 
 
 
--- obj.py
+++ obj.py
@@ -9,7 +9,7 @@ import numpy as np
 from scipy.integrate import odeint
 from make_model import make_model
 from allocate import allocate_parameters
-from setup_model import ref, prm, sel, agg, gps_born
+from setup_model import ref, prm, sel, agg, gps_born, likelihood_function
 from goveqs_basis import goveqs_basis2
 from get_addresses import get_addresses
 
@@ -31,26 +31,24 @@ p, r = allocate_parameters(x, p, r, xi)
 
 M = make_model(p, r, i, s, gps)
 
+
 init = np.zeros(i['nx'])
 seed = 1e-5
 init[i[('U', 'dom')]] = 1 - seed
 init[i[('I', 'dom')]] = seed
 
 def geq(t, in_):
-    return goveqs_basis2(t, in_, i, s, M, agg, sel, r, p)
+    return goveqs_basis2(t, in_, i, s, M, agg, sel, r, p).flatten()
 
 t0 = np.arange(2021)  # Adjust the time points accordingly
 soln0 = odeint(geq, init, t0, tfirst=True)
 
 
-
 dsol = np.diff(soln0, axis=0)
-incd2010 = dsol[t0[:2020] == 2010, i['aux']['inc'][0]] * 1e5
-
-incd2010 = dsol[t0 == 2010, i['aux']['inc'][0]] * 1e5
+incd2010 = dsol[t0[:-1] == 2010, i['aux']['inc'][0]] * 1e5
 incd2020 = dsol[-1, i['aux']['inc'][0]] * 1e5
 incd = dsol[-1, i['aux']['inc']] * 1e5
-mort = dsol[-1, i['aux']['mort']] * 1e5
+mort = dsol[-1, -1] * 1e5
 p_migrTB = incd[2] / incd[0]
 
 sfin = soln0[-1, :]
@@ -59,8 +57,9 @@ p_LTBI = np.sum(sfin[np.intersect1d(s['for'], [s['Lf'], s['Ls']])]) / np.sum(sfi
 sfin = soln0[-1, :]
 p_migrpopn = np.sum(sfin[s['for']]) / np.sum(sfin[:i['nstates']])
 
-if incd > 0.1:
-    out = calfn(incd2010, incd2020, mort, p_migrTB, p_migrpopn, p_LTBI)
+if np.any(incd > 0.1):
+    #out = calfn(incd2010, incd2020, mort, p_migrTB, p_migrpopn, p_LTBI)
+    out = likelihood_function(incd2010, incd2020, mort, p_migrTB, p_migrpopn, p_LTBI)
     aux = {
         'soln': soln0,
         'incd': dsol[np.where(t0 == 2010)[0][0]:, i['aux']['inc'][0]] * 1e5,
@@ -74,7 +73,3 @@ if incd > 0.1:
 else:
     out = -np.inf
     aux = np.nan
-
-
-print(t0.shape)
-print(dsol.shape)
