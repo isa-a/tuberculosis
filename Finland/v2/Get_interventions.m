196b0ea4f7b180fff3af6dd50374049f4ce4261f
Isa Ahmad <ia19@dide.local>
2024-05-30 17:51:23 +0100

    latest

--- Finland/v2/Get_interventions.m
+++ Finland/v2/Get_interventions.m
@@ -36,26 +36,32 @@ for ii = 1:size(xs,1)
     
     p2 = p0; r2 = r0;
     p2.migrTPT = 1;
-    r2.ACF = 3.91202*[1 1];
-    M2 = make_model(p2,r2,i,s,gps);
+    r2.ACF = 3.91202*[0 1];
+    M2 = make_model(p2,r2,i,s,gps); %ACF in elderly
 
     p3 = p0; r3 = r0;
     p3.migrTPT = 1;
-    r3.ACF = 3.91202*[1 1];
-    r3.TPT = 3.91202*[1 0];
+    r3.ACF = 3.91202*[0 1];
+    r3.TPT = 3.91202*[0 1]; %ACF in elderly and TPT in elderly
     M3 = make_model(p3,r3,i,s,gps);
     
     p4 = p0; r4 = r0;
     p4.migrTPT = 1;
-    r4.TPT = 3.91202*[1 1];
-    r4.ACF = 3.91202*[1 1];
+    r4.ACF = 3.91202*[1 1]; %ACF in everyone
+    r4.TPT = 3.91202*[0 1];
     M4 = make_model(p4,r4,i,s,gps);
+
+    p5 = p0; r5 = r0;
+    p5.migrTPT = 1;
+    r5.TPT = 3.91202*[1 1];
+    r5.ACF = 3.91202*[1 1]; %ACF and TPT in everyone
+    M5 = make_model(p5,r5,i,s,gps);
     
-    models = {M0, M2, M3, M4};
+    models = {M0, M2, M3, M4, M5};
     
     for mi = 1:length(models)
-        geq = @(t,in) goveqs_scaleup(t, in, i, M0, models{mi}, [2022 2025], agg, sel, r, p0);
-        [t,soln] = ode15s(geq, [2024:2037], init);
+        geq = @(t,in) goveqs_scaleup(t, in, i, M0, models{mi}, [2024 2029], agg, sel, r, p0);
+        [t,soln] = ode15s(geq, [2022:2041], init);
         
         sdiff = diff(soln,[],1);
         incsto(:,ii,mi) = sdiff(:,i.aux.inc(1))*1e5;
@@ -73,7 +79,7 @@ mat = permute(prctile(incsto,[2.5,50,97.5],2),[2,1,3]);
 cols = linspecer(size(mat,3));
 figure; lw = 3; fs = 14;
 
-xx = [2024:2036];
+xx = [2022:2040];
 for ii = 1:size(mat,3)
    plt = mat(:,:,ii);
    lg(ii,:) = plot(xx, plt(2,:), 'Color', cols(ii,:), 'linewidth', lw); hold on;
@@ -85,7 +91,7 @@ set(gca,'fontsize',fs);
 ylabel('Incidence per 100,000 population');
 xlabel('Year');
 
-legend(lg, 'Baseline incidence','Active case finding','Active case finding & TPT in young adults','Active case finding & TPT in young adults and the elderly','location','SouthWest');
+legend(lg, 'Baseline incidence','ACF in the elderly','ACF & TPT in the elderly','ACF in young adults and the elderly', 'ACF and TPT in everyone', 'location','SouthWest');
 
 % Show the proportions from different sources
 %tmp1 = prctile(props,[2.5,50,97.5],1);
