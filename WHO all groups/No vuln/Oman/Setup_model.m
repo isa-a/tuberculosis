d70b9a1ef4cff065535e02a16e7923bf642f639e
Isa Ahmad <ia19@dide.local>
2024-10-10 17:04:55 +0100

    Latest

--- WHO all groups/No vuln/Oman/Setup_model.m
+++ WHO all groups/No vuln/Oman/Setup_model.m
@@ -207,7 +207,7 @@ ref.i = i; ref.s = s; ref.xi = xi;
 prm.p = p; prm.r = r; prm.agg = agg; prm.sel = sel;
 
 prm.contmat_born = [1, 0.5; 0.5, 1; 0.2, 0.2];
-prm.contmat_age  = [1 0.3; 0.3 1];
+prm.contmat_age  = [1 0.1; 0.1 1];
 
 
 % go through each element
