c7979909a48c332c8221ab3a0e9db5bf42294509
isa-a <ia19@ic.ac.uk>
2025-06-05 21:39:28 +0100

    Latest

--- WHO all groups/England declining immigration/v3 no age/slidefig.m
+++ WHO all groups/England declining immigration/v3 no age/slidefig.m
@@ -3,7 +3,7 @@ obj = @(x) get_objective3(x, ref, prm, gps, prm.contmat, rin_vec, lhd);
 
 opts = odeset('RelTol', 1e-9, 'AbsTol', 1e-9);
 
-midpt = false; 
+midpt = true; 
 if midpt
     xs = x3;
 else
